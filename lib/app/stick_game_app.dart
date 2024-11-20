97ad7008d66db67dda4e71bf96319cf0b9616e66
Massterxvming <2260947414@qq.com>
2024-11-21 00:08:14 +0800

    build a painter

--- lib/app/stick_game_app.dart
+++ lib/app/stick_game_app.dart
@@ -1,25 +1,33 @@
-
-
 import 'dart:async';
 
 import 'package:flame/game.dart';
 import 'package:flame/src/components/core/component.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_game_1/modules/target_compontent.dart';
 
-class StickGameApp extends FlameGame{
-  final Paint paint = Paint()..color = const Color.fromARGB(255, 35, 26, 36);
+class StickGameApp extends FlameGame {
+  final Paint paint = Paint()..color = const Color.fromARGB(255, 106, 18, 116);
   final Path canvasPath = Path();
+
+  //角色
+  late TargetCompontent targetCompontent;
 
   @override
   Future<void>? onLoad() async {
-    canvasPath.addRect(Rect.fromLTWH(0,0,canvasSize.x,canvasSize.y));
-    return super.onLoad();
+    canvasPath.addRect(Rect.fromLTWH(0, 0, canvasSize.x, canvasSize.y));
 
+    // ignore: dead_code
+    targetCompontent =
+        TargetCompontent(position: Vector2(canvasSize.x / 2, canvasSize.y / 2));
+
+    return super.onLoad();
   }
 
   @override
-  void render(Canvas canvas){
+  void render(Canvas canvas) {
     super.render(canvas);
     canvas.drawPath(canvasPath, paint);
+
+    targetCompontent.render(canvas);
   }
 }
---
+++ lib/modules/target_compontent.dart
@@ -0,0 +1,20 @@
+
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class TargetCompontent {
+  final Vector2 position;
+  final double radius;
+  late Paint paint = Paint()..color = Colors.greenAccent;
+
+  TargetCompontent({
+    required this.position,
+    this.radius = 20,
+  });
+
+  void render(Canvas canvas){
+    canvas.drawCircle(position.toOffset(), radius, paint);
+  }
+}
